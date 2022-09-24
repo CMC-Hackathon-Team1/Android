c3fede6fa46feecf71bd39f2b4150c88eba8691b
Ginieee <kaj1226@naver.com>
2022-09-25 06:15:05 +0900

    [FIX]mypage retrofit

--- app/src/main/java/com/team1/projectteam1/domain/data/MypageRetroInterface.kt
+++ app/src/main/java/com/team1/projectteam1/domain/data/MypageRetroInterface.kt
@@ -1,5 +1,6 @@
 package com.team1.projectteam1.domain.data
 
+import com.team1.projectteam1.presentation.mypage.GetMypageResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -8,5 +9,5 @@ interface MypageRetroInterface {
     @GET("/users/{profileId}")
     fun getMyPage(
         @Path("profileId") profileId : Int
-    ) : retrofit2.Call<>
+    ) : retrofit2.Call<GetMypageResponse>
 }
