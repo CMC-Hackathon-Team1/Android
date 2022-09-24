76f19571c925a55e47614167c370c197691f6d42
한창희 <nimok97@gmail.com>
2022-09-25 04:59:16 +0900

    [FEAT] 홈 탭 레이아웃

---
+++ app/src/main/java/com/team1/projectteam1/domain/model/MyProfile.kt
@@ -0,0 +1,7 @@
+package com.team1.projectteam1.domain.model
+
+data class MyProfile(
+    val profileImage: String,
+    val name: String,
+    val isAddItem: Boolean
+)
---
+++ app/src/main/java/com/team1/projectteam1/domain/model/RelevantUser.kt
@@ -0,0 +1,6 @@
+package com.team1.projectteam1.domain.model
+
+data class RelevantUser(
+    val profileImage: String,
+    val name: String
+)
