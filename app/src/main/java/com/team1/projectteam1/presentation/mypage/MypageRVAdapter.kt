9fa7217f52cb3d1023a0b35b5b55250a2bef6c10
Ginieee <kaj1226@naver.com>
2022-09-25 01:13:27 +0900

    [FEAT]mypage fragment xml

---
+++ app/src/main/java/com/team1/projectteam1/presentation/mypage/MypageRVAdapter.kt
@@ -0,0 +1,22 @@
+package com.team1.projectteam1.presentation.mypage
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.team1.projectteam1.databinding.ItemMypageRvBinding
+
+class MypageRVAdapter() : RecyclerView.Adapter<MypageRVAdapter.ViewHolder>() {
+    inner class ViewHolder(val binding : ItemMypageRvBinding) : RecyclerView.ViewHolder(binding.root){
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
