604b90dffe99583f7ddcb4db43e0536ecab5e4fb
anna <mink141416@gmail.com>
2022-09-24 23:28:22 +0900

    [FEAT] 프로필사진 앨범에서 선택

--- app/src/main/java/com/team1/projectteam1/presentation/home/profile/ProfileActivity.kt
+++ app/src/main/java/com/team1/projectteam1/presentation/home/profile/ProfileActivity.kt
@@ -1,8 +1,15 @@
 package com.team1.projectteam1.presentation.home.profile
 
+
 import android.os.Bundle
+import android.Manifest
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.team1.projectteam1.databinding.ActivityProfileBinding
+
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -10,9 +17,31 @@ class ProfileActivity : AppCompatActivity() {
         ActivityProfileBinding.inflate(layoutInflater)
     }
 
+    private val permissionList = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+    private val checkPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { result ->
+        result.forEach {
+            if(!it.value) {
+                Toast.makeText(applicationContext, "권한 동의 필요!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private val readImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        Glide.with(this)
+            .load(uri)
+            .into(binding.profileIv)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        checkPermission.launch(permissionList)
+
+        binding.profileIv.setOnClickListener {
+            readImage.launch("image/*")
+        }
 
     }
 }
